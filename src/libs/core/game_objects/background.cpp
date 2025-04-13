62cef5f624af141c91bfeebea33a777c554f3d22
Denis Netahin <denis.netahin@yandex.ru>
2025-04-13 13:32:19 +0300

    add custom item with shader

--- src/libs/core/game_objects/background.cpp
+++ src/libs/core/game_objects/background.cpp
@@ -19,35 +19,6 @@ public:
         QSGMaterial* newMaterial, QSGMaterial* oldMaterial) override;
 };
 
-bool CustomShader::updateUniformData(RenderState& state, QSGMaterial* newMaterial, QSGMaterial* oldMaterial)
-{
-    bool changed = false;
-    QByteArray* buf = state.uniformData();
-    Q_ASSERT(buf->size() >= 84);
-
-    if (state.isMatrixDirty()) {
-        const QMatrix4x4 m = state.combinedMatrix();
-        memcpy(buf->data(), m.constData(), 64);
-        changed = true;
-    }
-
-    if (state.isOpacityDirty()) {
-        const float opacity = state.opacity();
-        memcpy(buf->data() + 64, &opacity, 4);
-        changed = true;
-    }
-
-    auto* customMaterial = static_cast<CustomMaterial*>(newMaterial);
-    if (oldMaterial != newMaterial || customMaterial->uniforms.dirty) {
-        memcpy(buf->data() + 68, &customMaterial->uniforms.zoom, 4);
-        memcpy(buf->data() + 72, &customMaterial->uniforms.center, 8);
-        memcpy(buf->data() + 80, &customMaterial->uniforms.limit, 4);
-        customMaterial->uniforms.dirty = false;
-        changed = true;
-    }
-    return changed;
-}
-
 //CustomMaterial
 class CustomMaterial : public QSGMaterial
 {
@@ -84,6 +55,36 @@ int CustomMaterial::compare(const QSGMaterial* o) const
     Q_ASSERT(o && type() == o->type());
     const auto* other = static_cast<const CustomMaterial*>(o);
     return other == this ? 0 : 1; // ### TODO: compare state???
+}
+
+//CustomShader
+bool CustomShader::updateUniformData(RenderState& state, QSGMaterial* newMaterial, QSGMaterial* oldMaterial)
+{
+    bool changed = false;
+    QByteArray* buf = state.uniformData();
+    Q_ASSERT(buf->size() >= 84);
+
+    if (state.isMatrixDirty()) {
+        const QMatrix4x4 m = state.combinedMatrix();
+        memcpy(buf->data(), m.constData(), 64);
+        changed = true;
+    }
+
+    if (state.isOpacityDirty()) {
+        const float opacity = state.opacity();
+        memcpy(buf->data() + 64, &opacity, 4);
+        changed = true;
+    }
+
+    auto* customMaterial = static_cast<CustomMaterial*>(newMaterial);
+    if (oldMaterial != newMaterial || customMaterial->uniforms.dirty) {
+        memcpy(buf->data() + 68, &customMaterial->uniforms.zoom, 4);
+        memcpy(buf->data() + 72, &customMaterial->uniforms.center, 8);
+        memcpy(buf->data() + 80, &customMaterial->uniforms.limit, 4);
+        customMaterial->uniforms.dirty = false;
+        changed = true;
+    }
+    return changed;
 }
 
 //CustomNode
